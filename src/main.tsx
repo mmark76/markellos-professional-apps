52c25dcc224a9baa8416b863fd681962a9919b36
Markides.Markellos <markellos.markides@gmail.com>
2026-07-25 01:36:24 +0300

    Apply full-width card text styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './footer-overrides.css'
+import './content-width-overrides.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
