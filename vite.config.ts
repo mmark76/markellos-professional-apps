71629ffbe60f19f1a8b667181e5f9e7d8e643384
Markides.Markellos <markellos.markides@gmail.com>
2026-07-25 00:39:15 +0300

    Generate build version from timestamp and commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const pad = (value: number) => String(value).padStart(2, '0')
+const buildDate = new Date()
+const buildStamp = [
+  buildDate.getUTCFullYear(),
+  pad(buildDate.getUTCMonth() + 1),
+  pad(buildDate.getUTCDate()),
+].join('') + '_' + [
+  pad(buildDate.getUTCHours()),
+  pad(buildDate.getUTCMinutes()),
+].join('')
+
+const shortCommit = (
+  process.env.CF_PAGES_COMMIT_SHA
+  ?? process.env.GITHUB_SHA
+  ?? 'local'
+).slice(0, 7)
+
+const buildVersion = `0.1.0_${buildStamp}_${shortCommit}`
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-build-version',
+      transformIndexHtml(html) {
+        return html.replace(
+          '</body>',
+          `<span class="build-version" aria-label="Build version">${buildVersion}</span></body>`,
+        )
+      },
+    },
+  ],
 })
