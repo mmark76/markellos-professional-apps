54d446dfd5a6157370ebf9d9818e5d3681bcd252
Markides.Markellos <markellos.markides@gmail.com>
2026-07-25 15:39:28 +0300

    Apply safe hero button visibility override

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './footer-overrides.css'
 import './content-width-overrides.css'
+import './hide-hero-buttons.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
